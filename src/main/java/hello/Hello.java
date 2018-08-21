7ec655a8eba7a57d8736d41cfb18de6c5ff99956
zhijie.txt <zhijie@qq.com>
2018-08-21 09:59:50 +0800

    第一次向github提交代码

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+/**
+ * Created by Semon.
+ * date:2018/8/17
+ * time:下午1:39
+ */
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/hello/User.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by Semon.
+ * date:2018/8/17
+ * time:下午2:24
+ */
+public class User {
+    String name;
+    int age;
+}
